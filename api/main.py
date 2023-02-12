a88e0435f0ff1871789bb0f33b5f337a1e9323d7
fthcknmz <fethicekinmez@gmail.com>
2023-02-13 00:25:21 +0300

    api is added

---
+++ api/main.py
@@ -0,0 +1,47 @@
+from fastapi import FastAPI, File, UploadFile
+import uvicorn
+import numpy as np
+from io import BytesIO
+from PIL import Image
+import tensorflow as tf
+
+MODEL = tf.keras.models.load_model("models/1")
+
+CLASS_NAMES = ['Tomato_bacterial_spot',
+                'Tomato_early_blight',
+                'Tomato_healthy',
+                'Tomato_late_blight',
+                'Tomato_leaf_mold',
+                'Tomato_spider_mites',
+                'Tomato_yellow_leaf_curl_virus']
+
+app = FastAPI()
+
+@app.get("/ping")
+async def ping():
+    return "PING!"
+
+
+def read_file_as_image(data) -> np.ndarray:
+    image = np.array(Image.open(BytesIO(data)))
+    return image
+
+@app.post("/predict")
+async def predict(
+    file: UploadFile = File(...)
+):
+    image = read_file_as_image(await file.read())
+    img_batch = np.expand_dims(image, 0)
+    
+    predictions = MODEL.predict(img_batch)
+
+    predicted_class = CLASS_NAMES[np.argmax(predictions[0])]
+    confidence = np.max(predictions[0])
+    return {
+        'class': predicted_class,
+        'confidence': float(confidence)
+    }
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host='localhost', port=8000)
